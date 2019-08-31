87fb8be330c897e8d0faf6aef7f9df029dd1a8b4
Jorge Falcón <39317414+jorgefalconcampos@users.noreply.github.com>
2019-08-31 12:34:04 -0500

    Update Raiz cuadrada (biseccion de Newton).cpp

--- U🍕/Raiz cuadrada (biseccion de Newton).cpp
+++ U🍕/Raiz cuadrada (biseccion de Newton).cpp
@@ -8,14 +8,13 @@ void Calcular(double y, double a, double b);
 double eval(double r, double x);
 double f(double a, double b, double x);
 
-double liminf = 0, limsup = 1000, raiz, aprox; 
+double numero, liminf = 0, limsup = 1000, raiz, aprox;
 int cont=0;
 
 int main() { MostrarOpc(); }
 
 void MostrarOpc()
 { 
-	double numero;
 	cout << "\n ESTEq PROGRAMA TE AYUDARA A SACAR LA RAIZ CUADRADA DE UN NUMERO";
 	cout << "\n\n Vamos a evaluar con el metodo de biseccion, usando la funcion (n)^2 - x \n\n  Donde: \n     n = aproximacion calculada \n     x = raiz a buscar"; 
 	cout << "\n\n Escribe un numero del 0 al 1,000: "; cin >> numero;
@@ -71,7 +70,7 @@ double f(double a, double b, double x) //evaluacion de las funciones
 {
 	double eval1 = ((a*a) - x); //evaluamos lim a
 	double eval2 = ((b*b) - x); //evaluamos lim b
-	double result = eval1*eval2; //multiplicamos la evaluacion. Si es < 0, la raiz está en ese intervalo
+	double result = eval1*eval2; //multiplicamos la evaluacion. Si es < 0, la raiz estÃ¡ en ese intervalo
 	cout << " \n   Evaluando: f(" << a << ") *  f(" << b << ") . . .";
 	if (result < 0) { cout << " [ f(" << a << ")* f(" << b << ") = " << result << " ]  < 0 "; }
 	else { cout << " [ f(" << a << ")* f(" << b << ") = " << result << " ]  > 0 "; }
